a67bbcb01e8c7cdde53cd7ed0ed8566e0406f018
ZhengPK <18300622503@163.com>
2026-04-01 20:12:53 +0800

    init

---
+++ src/AppService/env.ts
@@ -0,0 +1,10 @@
+const env = {
+    baseURL: process.env.REACT_APP_BASE_URL,
+    assetsPrefix: process.env.REACT_APP_ASSETS_PREFIX,
+    uploadURL: process.env.REACT_APP_UPLOAD_BASE_URL,
+    enableMockApi: process.env.REACT_APP_ENABLE_MOCK_API === 'true',
+};
+
+console.log("env", env);
+
+export default env;
